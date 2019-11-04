261b39d0943050fd8a07f4e270ddf86b86ec4591
Aditya Mishra <aditya.mishra@dynamix.co.in>
2019-11-04 20:22:12 +0530

    project init

---
+++ MyMajorCliniq001/majorenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aditya\documents\visual studio 2017\Projects\MyMajorCliniq001\MyMajorCliniq001\majorenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
